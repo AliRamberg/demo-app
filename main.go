45cf95d1a80ae62b0fe87117f9fcae89412f8604
AliRamberg <49831570+AliRamberg@users.noreply.github.com>
2023-07-18 17:17:51 +0300

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	myFigure := figure.NewColorFigure("AliRamberg is Awesome!!!", "larry3d", "yellow", true)
+	myFigure := figure.NewColorFigure("Someone is Awesome!!!", "larry3d", "yellow", true)
 	myFigure.Print()
 
 	if secret := os.Getenv("SECRET"); secret != "" {
